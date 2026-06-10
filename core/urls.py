b9831cadd4830b4052fac2349ef6f43bfe3536df
guilhermepinheiroo <guilhermepinheiro603@gmail.com>
2026-06-10 11:26:17 -0300

    Update urls

--- core/urls.py
+++ core/urls.py
@@ -1,8 +1,14 @@
 from django.urls import path
+from django.http import HttpResponse
 from app import views
 from app.healthcheck import HealthCheckView
 
+# Ignore favicon requests
+def favicon(request):
+    return HttpResponse(status=204)
+
 urlpatterns = [
+    path('favicon.ico', favicon),
     path('', views.home),
     path('health/', HealthCheckView.as_view()),
     path('curso/<int:id>/', views.curso),
